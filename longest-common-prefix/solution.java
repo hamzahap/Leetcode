a505b1f81f21918638f0a8b09eb866870085f40d
Hamzah Punjabi <hamzahqatar123@gmail.com>
2023-08-03 16:55:13 -0230

    Longest Common Prefix

---
+++ longest-common-prefix/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String a = strs[0];
+        for (int i=1; i<strs.length; i++){
+            while(strs[i].indexOf(a)!=0){
+                a = a.substring(0, a.length()-1);
+            }
+        }
+        return a;
+    }
+}
